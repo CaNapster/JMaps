c5bd38d1f45f1409386fc9d69d6ba7a46c2bf5ca
ca.napster <napster.ca@gmail.com>
2013-03-26 17:35:30 +0600

    startVersion

---
+++ src/JMaps.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: napster
+ * Date: 3/26/13
+ * Time: 5:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JMaps {
+}
